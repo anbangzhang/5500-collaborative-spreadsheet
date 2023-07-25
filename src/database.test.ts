e2259720bb87d0025f233c30bacc807065ba1b3d
anbang <zhang.anb@northeastern.edu>
2023-07-25 16:47:26 -0700

    add rest course's code to start up

---
+++ src/database.test.ts
@@ -0,0 +1,209 @@
+export { };
+import { Task, Database } from './database';
+import * as fs from 'fs';
+import * as path from 'path';
+
+
+
+describe('Task', () => {
+    let task: Task;
+
+    beforeEach(() => {
+        task = new Task('Test Task', 60, false);
+    });
+
+    describe('constructor', () => {
+        it('should set the name, time, and complete properties', () => {
+            expect(task.name).toBe('Test Task');
+            expect(task.time).toBe(60);
+            expect(task.complete).toBe(false);
+        });
+    });
+
+    describe('name', () => {
+        it('should get the name property', () => {
+            expect(task.name).toBe('Test Task');
+        });
+
+        it('should set the name property', () => {
+            task.name = 'Updated Task';
+            expect(task.name).toBe('Updated Task');
+        });
+    });
+
+    describe('time', () => {
+        it('should get the time property', () => {
+            expect(task.time).toBe(60);
+        });
+
+        it('should set the time property', () => {
+            task.time = 120;
+            expect(task.time).toBe(120);
+        });
+    });
+
+    describe('complete', () => {
+        it('should get the complete property', () => {
+            expect(task.complete).toBe(false);
+        });
+
+        it('should set the complete property', () => {
+            task.complete = true;
+            expect(task.complete).toBe(true);
+        });
+    });
+
+    describe('requestOwner', () => {
+        it('should set the owner property if it is empty', () => {
+            const result = task.requestOwner('Test User');
+            expect(result).toBe(true);
+            expect(task.owner).toBe('Test User');
+        });
+
+        it('should not set the owner property if it is already set', () => {
+            task.requestOwner('Test User');
+            const result = task.requestOwner('Another User');
+            expect(result).toBe(false);
+            expect(task.owner).toBe('Test User');
+        });
+    });
+});
+
+describe('Database', () => {
+    let database: Database;
+
+    beforeEach(() => {
+        database = new Database();
+    });
+
+    afterEach(() => {
+        database['_tasks'] = new Map<string, Task>();
+        database['_id'] = 0;
+        fs.unlinkSync(database['_filename']);
+    });
+
+    describe('constructor', () => {
+        it('should create a new database instance', () => {
+            expect(database).toBeDefined();
+        });
+
+        it('should load the tasks from the file', () => {
+            const taskName = 'Test Task';
+            database.addTask(taskName);
+            database = new Database();
+            expect(database.tasks.size).toBe(1);
+            const task = database.tasks.get('000000');
+            expect(task).toBeDefined();
+            expect(task?.name).toBe(taskName);
+
+        });
+    });
+
+    describe('addTask', () => {
+        it('should add a task to the list of tasks', () => {
+            const id = database.addTask('Test Task');
+            expect(database.tasks.size).toBe(1);
+            expect(database.tasks.get(id)).toBeDefined();
+            expect(database.tasks.get(id)?.name).toBe('Test Task');
+            expect(database.tasks.get(id)?.time).toBe(0);
+            expect(database.tasks.get(id)?.complete).toBe(false);
+        });
+
+        it('should generate a unique id for the task', () => {
+            const id1 = database.addTask('Test Task 1');
+            const id2 = database.addTask('Test Task 2');
+            expect(id1).not.toBe(id2);
+        });
+    });
+
+    describe('addUserToTask', () => {
+        it('should set the owner of the task to the specified user', () => {
+            const id = database.addTask('Test Task');
+            const result = database.addUserToTask(id, 'Test User');
+            expect(result).toBe(true);
+            expect(database.tasks.get(id)?.owner).toBe('Test User');
+        });
+
+        it('should not set the owner of the task if it is already set', () => {
+            const id = database.addTask('Test Task');
+            database.addUserToTask(id, 'Test User');
+            const result = database.addUserToTask(id, 'Another User');
+            expect(result).toBe(false);
+            expect(database.tasks.get(id)?.owner).toBe('Test User');
+        });
+
+        it('should return false if the task does not exist', () => {
+            const result = database.addUserToTask('000000', 'Test User');
+            expect(result).toBe(false);
+        });
+    });
+
+    describe('reset', () => {
+        it('should clear the list of tasks', () => {
+            database.addTask('Test Task');
+            database.reset();
+            expect(database.tasks.size).toBe(0);
+        });
+    });
+
+    describe('makeData', () => {
+        it('should create a list of tasks', () => {
+            database.makeData();
+            expect(database.tasks.size).toBe(5);
+        });
+    });
+
+    describe('removeUserFromTask', () => {
+        it('should clear the owner of the task', () => {
+            const id = database.addTask('Test Task');
+            database.addUserToTask(id, 'Test User');
+            database.removeUserFromTask(id, 'Test User');
+            expect(database.tasks.get(id)?.owner).toBe('');
+        });
+
+        it('should not clear the owner of the task if the user does not match', () => {
+            const id = database.addTask('Test Task');
+            database.addUserToTask(id, 'Test User');
+            database.removeUserFromTask(id, 'Another User');
+            expect(database.tasks.get(id)?.owner).toBe('Test User');
+        });
+
+        it('should do nothing if the task does not exist', () => {
+            database.removeUserFromTask('000000', 'Test User');
+            expect(database.tasks.size).toBe(0);
+        });
+    });
+
+    describe('addTimeToTask', () => {
+        it('should add time to the task if the user matches the owner', () => {
+            const id = database.addTask('Test Task');
+            database.addUserToTask(id, 'Test User');
+            const result = database.addTimeToTask(id, 'Test User', 30);
+            expect(result).toBe(true);
+            expect(database.tasks.get(id)?.time).toBe(30);
+        });
+
+        it('should not add time to the task if the user does not match the owner', () => {
+            const id = database.addTask('Test Task');
+            database.addUserToTask(id, 'Test User');
+            const result = database.addTimeToTask(id, 'Another User', 30);
+            expect(result).toBe(false);
+            expect(database.tasks.get(id)?.time).toBe(0);
+        });
+
+        it('should do nothing if the task does not exist', () => {
+            const result = database.addTimeToTask('000000', 'Test User', 30);
+            expect(result).toBe(false);
+        });
+    });
+
+    describe('named constructor', () => {
+        it('should be able to create a new database with a name', () => {
+            const database1 = new Database('test1');
+            const database2 = new Database('test2');
+            expect(database1['_filename']).toBe(path.join(__dirname, 'documents', 'test1.json'));
+            expect(database2['_filename']).toBe(path.join(__dirname, 'documents', 'test2.json'));
+        });
+
+    });
+});
---
+++ src/database.ts
@@ -0,0 +1,171 @@
+
+// the file is a JSON file
+
+import exp from 'constants';
+import * as fs from 'fs';
+import * as path from 'path';
+import Task from './task';
+
+// a class that stores a list of tasks.  A task has a name, a time allocated to it, and a completion flag
+// the class also has methods to add, remove, and update tasks
+// when a new task is added it generates a unique id for the task (6 digit string) and returns it
+// the class also has methods to save and load the list of tasks to a file
+// the default location for the file is in the same directory as the executable
+// the file name for the data base is tasks.json 
+// the tasks are stored in a map that is indexed by the task id
+// the database provides different documents.   The location of the 
+// documents is in the directory "documents" in the same directory as the executable
+
+class Database {
+    private _tasks: Map<string, Task>;
+    private _filename: string;
+    private _id: number = 0;
+    private _documentDirectory: string = path.join(__dirname, "documents");
+
+    // default document name is tasks
+    constructor(documentName: string = "tasks") {
+        this._tasks = new Map<string, Task>();
+        // remove any / or \ from the document name
+        documentName = documentName.replace(/[\/\\]/g, "");
+
+        this._filename = path.join(this._documentDirectory, documentName + ".json");
+        this._load();
+        this._initializeDirectory();
+    }
+
+    private _initializeDirectory() {
+        if (!fs.existsSync(this._documentDirectory)) {
+            fs.mkdirSync(this._documentDirectory, { recursive: true });
+        }
+    }
+
+
+    private _load() {
+        try {
+            let data = fs.readFileSync(this._filename, 'utf8');
+            let tasks = JSON.parse(data);
+            for (let task of tasks) {
+                let newTask = new Task(task._name, task._time, task._complete);
+                this._tasks.set(task._id, newTask);
+            }
+            this._id = tasks.length;
+        } catch (err) {
+            // create the empty file
+            this._save();
+        }
+    }
+
+    private _save() {
+        let tasks = [];
+        for (let [id, task] of this._tasks) {
+            tasks.push({
+                _id: id,
+                _name: task.name,
+                _time: task.time,
+                _complete: task.complete
+            });
+        }
+        let data = JSON.stringify(tasks);
+        fs.writeFileSync(this._filename, data);
+    }
+
+    private _generateId(): string {
+        // pad the id with leading zeros
+        let id = this._id.toString().padStart(6, "0");
+        this._id++;
+        return id;
+    }
+
+
+    public get tasks(): Map<string, Task> {
+        return this._tasks;
+    }
+
+    public reset() {
+        this._tasks.clear();
+        this._id = 0;
+        this._save();
+    }
+
+    public makeData() {
+        this.reset();
+        this.addTask("Task 1");
+        this.addTask("Task 2");
+        this.addTask("Task 3");
+        this.addTask("Task 4");
+        this.addTask("Task 5");
+    }
+
+    public addTask(name: string): string {
+        let id = this._generateId();
+        let task = new Task(name, 0, false);
+        task.id = id;
+        this._tasks.set(id, task);
+        this._save();
+        return id;
+    }
+
+    public deleteTask(id: string, user: string): boolean {
+        let task = this._tasks.get(id);
+        if (task) {
+            if (task.owner === user) {
+                this._tasks.delete(id);
+                this._save();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public addUserToTask(taskId: string, user: string): boolean {
+        console.log(`>>>>>>>> attempting to assign ${user} to ${taskId}`);
+        let task = this._tasks.get(taskId);
+        if (task) {
+            const result = task.requestOwner(user);
+            if (result) {
+                this._save();
+            }
+            return result;
+        }
+        return false;
+    }
+
+    public removeUserFromTask(taskId: string, user: string) {
+        let task = this._tasks.get(taskId);
+        if (task) {
+            if (task.owner == user) {
+                task.clearOwner();
+                this._save();
+            }
+        }
+    }
+
+    public addTimeToTask(taskId: string, userId: string, time: number): boolean {
+        let task = this._tasks.get(taskId);
+        if (task) {
+            if (task.owner == userId) {
+                task.time += time;
+                console.log(`added ${time} to ${taskId} for ${userId}`);
+                this._save();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public markTaskComplete(taskId: string, userId: string): boolean {
+        let task = this._tasks.get(taskId);
+        if (task) {
+            if (task.owner == userId) {
+                task.complete = true;
+                this._save();
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+}
+
+export { Task, Database }
